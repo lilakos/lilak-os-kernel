7b53bd9d7824b72aadb798d90892bdb7a1b19c59
lilakos <31805615+lilakos@users.noreply.github.com>
2018-08-28 07:19:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,14 @@
-#C
-nasm -felf32 kernel/boot/kernel.asm -o obj/boot.o
-
-gcc -m32 -c kernel/kernel/kernel.c -I kernel/include -o obj/kernelc.o
-ld -m elf_i386 -T kernel/link.ld -o kernel.bin obj/boot.o obj/kernelc.o 
-
-#Linking
-
-#Making the disk image
-dd if=obj/bootloader/boot.bin of=LilakOS.img bs=512 conv=notrunc
-dd if=obj/stage2/stage2.bin of=LilakOS.img bs=512 seek=1 conv=notrunc
-dd if=obj/staticindex/index.bin of=LilakOS.img bs=512 conv=notrunc seek=2097151
-dd if=kernel.bin of=LilakOS.img bs=512 conv=notrunc seek=3 
-#Disassembling
-ndisasm obj/bootloader/boot.bin >  disassembled/bootloader/boot.dis
-ndisasm obj/stage2/stage2.bin >  disassembled/stage2/stage2.dis
-
-#Decompiling
-objdump -D  -m i386 kernel.bin > kernel.dis
-
-#Done!
-echo Done!
+cd Kernel
+nasm -f elf32 boot.asm -o ../obj/Kernel/boot.o
+nasm -f elf32 src/GDT.asm -o ../obj/Kernel/GDT.o
+nasm -f elf32 src/CPUID.asm -o ../obj/Kernel/CPUID.o
+gcc -c Kernel.c -o ../obj/Kernel/Kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -m32 -I include
+gcc -T linker.ld -o ../LilakOSKernelImage.LEF -ffreestanding -O2 -nostdlib -m32 -z max-page-size=0x1000 ../obj/Kernel/boot.o ../obj/Kernel/GDT.o ../obj/Kernel/CPUID.o ../obj/Kernel/Kernel.o
+cd ../
+#decompile
+objdump -D -m i386 LilakOSKernelImage.LEF > kernel.dis
+#disk image making
+dd if=obj/boot/boot.bin of=LilakOS.img bs=512 seek=0 count=1 conv=notrunc
+dd if=obj/boot/stage2.bin of=LilakOS.img bs=512 seek=1 count=2 conv=notrunc
+dd if=obj/index/staticindex.sta of=LilakOS.img bs=512 seek=4194303 count=1 conv=notrunc
+dd if=LilakOSKernelImage.LEF of=LilakOS.img bs=512 seek=4 conv=notrunc
